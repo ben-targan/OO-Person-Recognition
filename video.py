b9527b92a0e091900cd70e191edd0e19ea3b956e
Ben Targan <ben.targan@gmail.com>
2018-06-19 14:58:07 -0700

    cleaning up

--- video.py
+++ video.py
@@ -1,9 +1,8 @@
-from __future__ import print_function
+# Ben Targan
 from imutils.object_detection import non_max_suppression
 from imutils import paths
 import numpy as np
 import cv2
-import argparse
 import imutils
 
 
@@ -15,32 +14,30 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    
     frame = imutils.resize(frame, width=min(400, frame.shape[1]))
     orig = frame.copy()
- 
+
     # detect people in the frame
-    #use scale to balance speed / accuracy. larger = more speed.
-    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),
-        padding=(8, 8), scale=1.15)
- 
+    # use scale and winStride to balance speed / accuracy.
+    # larger = more speed.
+    (rects, weights) = hog.detectMultiScale(frame, winStride=(8, 8), padding=(8, 8), scale=1.25)
+
     # draw the original bounding boxes
     for (x, y, w, h) in rects:
         cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
- 
+
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
     # boxes that are still people
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
- 
+
     # draw the final bounding boxes
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
- 
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
