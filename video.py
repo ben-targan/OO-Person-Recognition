60f59ffbf85e4eb1d4a35a3eeca28bc158e104e6
Ben Targan <ben.targan@gmail.com>
2018-06-19 15:18:48 -0700

    adhering to PEP8

--- video.py
+++ video.py
@@ -1,9 +1,10 @@
 # Ben Targan
-from imutils.object_detection import non_max_suppression
-from imutils import paths
 import numpy as np
 import cv2
+#imutils can be found at: https://github.com/jrosebr1/imutils
 import imutils
+from imutils.object_detection import non_max_suppression
+from imutils import paths
 
 
 cap = cv2.VideoCapture(0)
@@ -11,28 +12,28 @@ hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 while(True):
-    # Capture frame-by-frame
+    # Capture from video frame-by-frame
     ret, frame = cap.read()
 
     frame = imutils.resize(frame, width=min(400, frame.shape[1]))
     orig = frame.copy()
 
-    # detect people in the frame
-    # use scale and winStride to balance speed / accuracy.
-    # larger = more speed.
+    # Detect people in the frame
+    # Use scale and winStride to balance speed / accuracy.
+    # Larger = more speed.
     (rects, weights) = hog.detectMultiScale(frame, winStride=(8, 8), padding=(8, 8), scale=1.25)
 
-    # draw the original bounding boxes
+    # Draw the original bounding boxes
     for (x, y, w, h) in rects:
         cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    # apply non-maxima suppression to the bounding boxes using a
-    # fairly large overlap threshold to try to maintain overlapping
-    # boxes that are still people
+    # Apply non-maxima suppression to the bounding boxes using a
+    # Fairly large overlap threshold to try to maintain overlapping
+    # Boxes that are still people
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-    # draw the final bounding boxes
+    # Draw the final bounding boxes on the frame
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
@@ -41,6 +42,6 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# When everything is finished, release the capture
 cap.release()
 cv2.destroyAllWindows()
